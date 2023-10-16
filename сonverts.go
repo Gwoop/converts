e308aa4f267338b5108f6aaf893f4c6861fb2be2
Gwoop <gwooparap@gmail.com>
2023-10-16 16:14:40 +0300

    создании библиотеки для конвертов

---
+++ сonverts.go
@@ -0,0 +1,48 @@
+package сonverts
+
+import (
+	"github.com/google/uuid"
+	"strconv"
+)
+
+func StringToInt64(str string) (number int64) {
+	i, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		panic(err)
+		return
+	}
+	return i
+}
+
+func StringToInt(str string) (number int) {
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		panic(err)
+		return
+	}
+	return i
+}
+
+func IntToString(number int) (str string) {
+	t := strconv.Itoa(number)
+	return t
+}
+
+func IntToInt64(number int) (number64 int64) {
+	number64 = int64(number)
+	return number64
+}
+
+func StringToUuid(str string) (UUID uuid.UUID) {
+	UUID = uuid.Must(uuid.Parse(str))
+	return UUID
+}
+
+func UintToInt(uNumber uint) (intResult int) {
+	intResult = int(uNumber)
+	return intResult
+}
+func IntToUint(Number int) (uintResult uint) {
+	uintResult = uint(Number)
+	return uintResult
+}
